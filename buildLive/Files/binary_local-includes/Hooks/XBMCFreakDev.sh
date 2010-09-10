34f80ff4f897e47894f5efc5ffa5719a91c37cc9
root <root@dhcppc9.lokaal>
2010-09-10 12:00:11 +0200

    fixed devscript

--- buildLive/Files/binary_local-includes/Hooks/XBMCFreakDev.sh
+++ buildLive/Files/binary_local-includes/Hooks/XBMCFreakDev.sh
@@ -46,10 +46,10 @@ chown -R $xbmcUser:$xbmcUser /home/$xbmcUser/.xbmc
 #nfs shares
 apt-get update
 aptitude -y install nfs-common
-echo "192.168.0.111:/volume1/misc/Videos /home/xbmc/Test nfs defaults 0 0" >> /etc/fstab
+echo "192.168.0.111:/volume1/misc/Videos /home/xbmc/Video nfs defaults 0 0" >> /etc/fstab
 echo "192.168.0.111:/volume1/misc/Music /home/xbmc/Music nfs defaults 0 0" >> /etc/fstab
 echo "192.168.0.111:/volume1/misc/Pictures /home/xbmc/Pictures nfs defaults 0 0" >> /etc/fstab
-echo "192.168.0.111:/volume1/misc/TV\ Shows /home/xbmc/TV\ Shows nfs defaults 0 0" >> /etc/fstab
+echo "192.168.0.111:/volume1/misc/TV\040Shows/ /home/xbmc/TV\040Shows/ nfs defaults 0 0" >> /etc/fstab
 echo "192.168.0.111:/volume1/downloads /home/xbmc/Downloads nfs defaults 0 0" >> /etc/fstab
 mount -a
 
